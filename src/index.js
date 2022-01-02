db8f9761efb1b92b13630505b6cb1daa4b2e8b1c
yewon <yewon1914@gmail.com>
2022-01-02 22:49:43 +0900

    Fragment test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import ReactDOM from 'react-dom';
 
 const product = 'Macbook'
+const model = "Air";
+const item = product + model;
+const imageUrl = 'https://forum-creallo.s3.dualstack.ap-northeast-2.amazonaws.com/original/1X/5e0d906585d6dbbf7f3c9997484eee594bd01da1.jpeg'
 
 ReactDOM.render(
-  <h1>나만의 {product.toUpperCase()} 주문하기</h1>,
+  <>
+  <h1>나만의 {product.toUpperCase()} 주문하기</h1>
+  <img src={imageUrl} alt="어피치" />
+  <button onClick={handleClick}>확인</button>
+  </>,
   document.getElementById('root')
 );
