d686f24df621ec14bb0c75369332ce1009fbbcbd
yewon <yewon1914@gmail.com>
2022-01-15 14:31:18 +0900

    start dicegame project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const product = 'Macbook'
-const model = "Air";
-const item = product + model;
-const imageUrl = 'https://forum-creallo.s3.dualstack.ap-northeast-2.amazonaws.com/original/1X/5e0d906585d6dbbf7f3c9997484eee594bd01da1.jpeg'
-
-ReactDOM.render(
-  <>
-  <h1>나만의 {product.toUpperCase()} 주문하기</h1>
-  <img src={imageUrl} alt="어피치" />
-  <button onClick={handleClick}>확인</button>
-  </>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App /> , document.getElementById('root'));
